a8fdb1ee92597db3dbe5bf1debd6c035a9d8e7f1
wanglanwei <dreamcatchwang1991@gmail.com>
2017-09-01 01:48:57 +0800

    fix blueprint.py and testcase.

--- tests.py
+++ tests.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*_
+
+from __future__ import print_function
+
+import nose
+import tornado.web
+
+from tornadoblueprint.blueprint import Blueprint
+
+
+def setup_func():
+    pass
+
+
+def teardown_func():
+    pass
+
+
+@nose.with_setup(setup_func, teardown_func)
+def test_blueprint():
+    blueprint = Blueprint('*.example.com', '/users')
+
+    @blueprint.route('/list')
+    class DemoHandler(tornado.web.RequestHandler):
+
+        def get(self):
+            self.write('demo handler.')
+
+
+if __name__ == '__main__':
+    nose.run()
--- tornadoblueprint/blueprint.py
+++ tornadoblueprint/blueprint.py
@@ -20,7 +20,7 @@ class BlueprintMeta(type):
 
     def __new__(metacls, cls_name, bases, namespace):
         _class = super(BlueprintMeta, metacls).__new__(
-                cls_name, bases, namespace)
+                metacls, cls_name, bases, namespace)
         metacls.derived_class.append(_class)
         return _class
 
@@ -45,11 +45,11 @@ class Blueprint(object):
         self.rules = []
 
     def route(self, uri):
-        def decorator(func):
-            @functools.wraps(func)
+        def decorator(handler):
+            @functools.wraps(handler)
             def wrapper(*args, **kwargs):
-                self.rules.append((self.prefix + uri, ))
-                res = func(*args, **kwargs)
+                self.rules.append((self.prefix + uri, handler))
+                res = handler(*args, **kwargs)
                 return res
             return wrapper
         return decorator
@@ -61,9 +61,9 @@ class HotSwapApplication(tornado.web.Application):
         if args and args[0] \
                 and isinstance(args[0], tornado.web.Application):
             self.app = args[0]
-        else:
-            self.app = super(
-                HotSwapApplication, self).__init__(*args, **kwargs)
+            return
+
+        self.app = super(HotSwapApplication, self).__init__(*args, **kwargs)
 
     @classmethod
     def proxy(cls, app):
